1245ede4d52c4d8f331177a642f1907b9abba3f0
jbresky <joaquin_bresky@hotmail.com>
2022-09-30 00:06:16 -0300

    mixins

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
 module.exports = {
     module: {
-     rules: [{
-        test: /\.png$/i,
-        type: 'asset/resource'
-     }]   
+        rules: [
+            {
+                test: /\.(png | jpg | jpeg)$/,
+                type: 'asset'
+            }, 
+            {
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+                test: /\.(css | scss | sass)$/i
+            }]
     },
     mode: "development"
 }
